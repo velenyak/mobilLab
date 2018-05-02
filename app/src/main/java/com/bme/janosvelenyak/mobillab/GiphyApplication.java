61d18f4ccc72b98bf5ad447934e8aeb880bee830
Janos Velenyak <janos.velenyak@gmail.com>
2018-05-02 02:03:59 +0200

    fix versions

--- app/src/main/java/com/bme/janosvelenyak/mobillab/GiphyApplication.java
+++ app/src/main/java/com/bme/janosvelenyak/mobillab/GiphyApplication.java
@@ -1,8 +1,25 @@
 package com.bme.janosvelenyak.mobillab;
 
+import android.app.Application;
+
+import com.bme.janosvelenyak.mobillab.ui.UIModule;
+
 /**
  * Created by janosvelenyak on 2018. 04. 20..
  */
 
-public class GiphyApplication {
+public class GiphyApplication extends Application {
+
+    public static GiphyApplicationComponent injector;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        injector =
+                DaggerGiphyApplicationComponent.builder().
+                        uIModule(
+                                new UIModule(this)
+                        ).build();
+    }
 }
--- app/src/main/java/com/bme/janosvelenyak/mobillab/ui/UIModule.java
+++ app/src/main/java/com/bme/janosvelenyak/mobillab/ui/UIModule.java
@@ -1,6 +1,13 @@
 package com.bme.janosvelenyak.mobillab.ui;
 
+import android.content.Context;
+
+import com.bme.janosvelenyak.mobillab.ui.main.MainPresenter;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
 
 /**
  * Created by janosvelenyak on 2018. 04. 20..
@@ -8,4 +15,20 @@ import dagger.Module;
 
 @Module
 public class UIModule {
+    private Context context;
+
+    public UIModule(Context context) {
+        this.context = context;
+    }
+
+    @Provides
+    public Context provideContext() {
+        return context;
+    }
+
+    @Provides
+    @Singleton
+    public MainPresenter provideMainPresenter() {
+        return new MainPresenter();
+    }
 }
